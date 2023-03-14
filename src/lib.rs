0bfbdf2c8927553b0367386b7c62285831f18238
JustFoxx <justfoxxo@outlook.com>
2023-03-14 15:18:56 +0100

    Changed tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,27 @@ mod tests {
     use crate::event::*;
     #[test]
     fn it_works() {
+        fn invoker(entries: &Vec<&dyn Fn(i32)>) -> impl Fn(i32) -> i32 {
+            let mut x = 1;
+            for entry in entries {
+                x *= 3;
+                (entry)(x);
+            };
+            move |y| {
+                x * y
+            }
+        }
+
         let mut event_test = EventKey::new(Box::new(
-                |entries: &Vec<&dyn Fn(i32)>| -> u32 {
-                    let mut x = 1;
-                    for entry in entries {
-                        x*=3;
-                        (entry)(x);
-                    }
-                    entries.len() as u32
-                }
-            ));
+                invoker,
+        ));
         let test = &|x: i32| {
             println!("Hello {}", x);
         };
         event_test.on_event(test);
         event_test.on_event(test);
         event_test.on_event(test);
-        let result = event_test.invoker();
-        assert_eq!(result, 3);
+        let result = event_test.invoker()(2);
+        assert_eq!(result, 54);
     }
 }
